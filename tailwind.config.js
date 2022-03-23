674515c5253d184d6c95b9c6a5e77fc58c5aa2ed
YammyShep <jdbuelow@gmail.com>
2022-03-23 00:50:49 -0500

    added link to source code, pronoun badges and lorem ipsum

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,6 @@ module.exports = {
   content: [
     "./src/**/*.{html,js,ts,jsx,tsx,wasm}",
   ],
-  theme: {
-    extend: {},
-    colors: {
-      bgprimary: '#332E3C',
-      bgsecondary: '#4E475C',
-      primary: '#B298DC',
-      secondary: '#A663CC',
-      trinary: '#8BE8CB',
-      accentprimary: '#E03616',
-      accentsecondary: '#F0816A'
-    }
-  },
   plugins: [
     require("daisyui")
   ],
